fc10ed75e146518325684a7b7da3f617765b39c1
Tom Noonan II <root@tjnii.com>
2013-07-20 18:43:48 -0500

    Added apt.conf to disable default install of suggested and recommended packages

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,4 +4,17 @@ class miscfiles {
     ensure => file,
     content => template("miscfiles/motd.erb"),
   }
+
+  case $operatingsystem {
+    debian, ubuntu: {
+      # Apt.conf
+      file { "/etc/apt.conf":
+        ensure  => file,
+        owner   => root,
+        group   => root,
+        mode    => 644,
+        source  => "puppet:///modules/miscfiles/apt.conf",
+      }
+    }
+  }
 }
