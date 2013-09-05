bb05ca43dfdc3e890a5540bea5f406538175d075
Tom Noonan II <root@tjnii.com>
2013-09-05 14:25:57 -0500

    Forking apt.conf to a packagemanagerconfig module

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,19 +2,10 @@ class miscfiles {
   case $operatingsystem {
     debian, ubuntu: {
       $motdflavor = $lsbdistdescription
-      
-      # Apt.conf
-      file { "/etc/apt.conf":
-        ensure  => file,
-        owner   => root,
-        group   => root,
-        mode    => 644,
-        source  => "puppet:///modules/miscfiles/apt.conf",
-      }
     }
 
     default: {
-      $motdflavor = "$operatingsystem $operatingsystemrelease"
+      $motdflavor = "$operatingsystem/$operatingsystemrelease"
     }
   }
 
